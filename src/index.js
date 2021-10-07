18f623473d0dde702b9cdcb037a6a6a4bb69ce1b
Gabriel Souza <gabrie.ssouza@gmail.com>
2021-10-07 18:59:05 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
-import styles from './styles.module.css'
+import styled from 'styled-components'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+export const AwesomeButton = styled.button`
+  background-color: black;
+  color: white;
+  border: none;
+  padding: 0.75rem 1.5rem;
+  cursor: pointer;
+  transition: background-color .2s;
+  &:hover {
+    background-color: gray;
+  }
+`
